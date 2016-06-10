cd21251d6798c6a0f2c14b641116aea3c89f8feb
Alex Windett <alex@bigfish.co.uk>
2016-06-10 10:55:46 +0100

    adding in football api

--- src/tasks/notify.js
+++ src/tasks/notify.js
@@ -5,6 +5,7 @@ const _ = require('lodash')
 const config = require('../config')
 const trending = require('github-trending')
 const Botkit = require('botkit')
+const http = require('http');
 
 var controller = Botkit.slackbot({})
 var bot = controller.spawn()
@@ -13,27 +14,38 @@ bot.configureIncomingWebhook({ url: config('WEBHOOK_URL') })
 
 const msgDefaults = {
   response_type: 'in_channel',
-  username: 'Starbot',
+  username: 'FOOTIEBOT',
   icon_emoji: config('ICON_EMOJI')
 }
 
-trending('javascript', (err, repos) => {
-  if (err) throw err
+// trending('javascript', (err, repos) => {
+//   if (err) throw err
+//
+//   var attachments = repos.slice(0, 5).map((repo) => {
+//     return {
+//       title: `${repo.owner}/${repo.title} `,
+//       title_link: repo.url,
+//       text: `_${repo.description}_\n${repo.language} • ${repo.star}`,
+//       mrkdwn_in: ['text', 'pretext']
+//     }
+//   })
+// })
+var url = 'http://api.football-data.org/v1/soccerseasons/424/fixtures';
+http.get(url, function(res) {
+  // console.log("Got response: " + res.statusCode);
 
-  var attachments = repos.slice(0, 5).map((repo) => {
-    return {
-      title: `${repo.owner}/${repo.title} `,
-      title_link: repo.url,
-      text: `_${repo.description}_\n${repo.language} • ${repo.star}`,
-      mrkdwn_in: ['text', 'pretext']
-    }
-  })
+  res.on("data", function(chunk) {
+    console.log("BODY: " + chunk);
 
-  let msg = _.defaults({ attachments: attachments }, msgDefaults)
+    let msg = _.defaults({ attachments: chunk }, msgDefaults)
 
-  bot.sendWebhook(msg, (err, res) => {
-    if (err) throw err
+    bot.sendWebhook(msg, (err, res) => {
+      if (err) throw err
 
-    console.log(`\n🚀  Starbot report delivered 🚀`)
-  })
-})
+      console.log(`\n🚀  Starbot report delivered 🚀`)
+    })
+
+  });
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
