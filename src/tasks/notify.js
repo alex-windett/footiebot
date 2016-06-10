96397b20a76b8fa6ad58d8f4696ff2f019dda133
Alex Windett <alex@bigfish.co.uk>
2016-06-10 11:04:09 +0100

    testing response

--- src/tasks/notify.js
+++ src/tasks/notify.js
@@ -5,7 +5,6 @@ const _ = require('lodash')
 const config = require('../config')
 const trending = require('github-trending')
 const Botkit = require('botkit')
-const http = require('http');
 
 var controller = Botkit.slackbot({})
 var bot = controller.spawn()
@@ -18,26 +17,24 @@ const msgDefaults = {
   icon_emoji: config('ICON_EMOJI')
 }
 
-// trending('javascript', (err, repos) => {
-//   if (err) throw err
-//
-//   var attachments = repos.slice(0, 5).map((repo) => {
-//     return {
-//       title: `${repo.owner}/${repo.title} `,
-//       title_link: repo.url,
-//       text: `_${repo.description}_\n${repo.language} • ${repo.star}`,
-//       mrkdwn_in: ['text', 'pretext']
-//     }
-//   })
-// })
-var url = 'http://api.football-data.org/v1/soccerseasons/424/fixtures';
 http.get(url, function(res) {
   // console.log("Got response: " + res.statusCode);
 
   res.on("data", function(chunk) {
     console.log("BODY: " + chunk);
 
-    let msg = _.defaults({ attachments: chunk }, msgDefaults)
+    var attachments = repos.slice(0, 5).map((repo) => {
+      return {
+        title: `hello`,
+        mrkdwn_in: ['text', 'pretext']
+        // title: `${repo.owner}/${repo.title} `,
+        // title_link: repo.url,
+        // text: `_${repo.description}_\n${repo.language} • ${repo.star}`,
+        // mrkdwn_in: ['text', 'pretext']
+      }
+    })
+
+    let msg = _.defaults({ attachments: attachments }, msgDefaults)
 
     bot.sendWebhook(msg, (err, res) => {
       if (err) throw err
